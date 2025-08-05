e7e7f345b99af10b6b42a4d5904c7b18c85cae9e
Julieta Farina <julifaier@gmail.com>
2025-08-04 21:47:33 -0300

    firt commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+const litado = ref(8)
+
+async function cargar() {
+  let product = await fetch(
+    'https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products/',
+  )
+  litado.value = await product.json()
+}
+cargar()
+</script>
+
+<template>
+  <ul>
+    <li v-for="(elemento, indice) in listado" :key="indice">{{ elemento.title }}</li>
+  </ul>
+</template>
+
+<style scoped></style>
